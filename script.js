4fd48d38e7b309c416e809adc66c24c7eba9f994
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-06-01 19:02:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (event) => {
+	event.preventDefault();
+	textVal = text.value;
+	delayVal = delay.value;
+	console.log(textVal,delayVal);
+})
